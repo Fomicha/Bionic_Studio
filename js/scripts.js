c1a01d63bf3362655e0ea781e1a93d617445cfa7
Fomi4 <fomich.aleksandr@gmail.com>
2016-05-25 14:18:22 +0300

    scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Slider
 $(document).ready(function(){
    $('.multiple-items').slick({
       infinite: true,
@@ -30,10 +31,51 @@ $(document).ready(function(){
            slidesToScroll: 1
          }
        }
-   // You can unslick at a given breakpoint now by adding:
-   // settings: "unslick"
-   // instead of a settings object
-    ]
+        ]
+    });
 });
-   });
+
+// Scroll
+$(document).ready(function(){
+var anchors = [];
+var currentAnchor = -1;
+var isAnimating  = false;
+
+$(function(){
+    function updateAnchors() {
+        anchors = [];
+        $('.anchor').each(function(i, element){
+            anchors.push( $(element).offset().top );
+        });
+    }
+    
+    $('body').on('mousewheel', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        if( isAnimating ) {
+            return false;
+        }
+        isAnimating  = true;
+        // Increase or reset current anchor
+        if( e.originalEvent.wheelDelta >= 0 ) {
+            currentAnchor--;
+        }else{
+            currentAnchor++;
+        }
+        if( currentAnchor > (anchors.length - 1) 
+           || currentAnchor < 0 ) {
+            currentAnchor = 0;
+        }
+        isAnimating  = true;
+        $('html, body').animate({
+            scrollTop: parseInt( anchors[currentAnchor] )
+        }, 500, 'swing', function(){
+            isAnimating  = false;
+        });
+    });
+
+    updateAnchors();   
+    
+});
+});
 
